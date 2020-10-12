ee57b52460ed10c15957ea874b2e16a8ab5fc77d
holtzermann17 <joseph.corneli@hyperreal.enterprises>
2020-10-12 12:55:28 +0100

    include org files here per Cameron's request

---
+++ _firn/layouts/default.clj
@@ -0,0 +1,18 @@
+(defn default
+  [{:keys [render partials title build-url]}]
+  (let [{:keys [head]} partials]
+    [:html
+     (head build-url)
+     [:body
+      [:main
+       [:article.content
+        ;; [:div (render :toc)] ;; Optional; add a table of contents
+        [:h1 title] 
+        [:div (render :file)]
+        ;; if backlinks exist, store them in a let bindings.
+        (when-let [backlinks (render :backlinks)] 
+          [:div
+           [:hr]
+           [:div.backlinks
+            [:h4 "Backlinks to this document:"]
+            backlinks]])]]]]))
