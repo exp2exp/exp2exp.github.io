6c182b9e0c2d64dbd559f8981bae3552509ea88d
holtzermann17 <joseph.corneli@hyperreal.enterprises>
2021-01-04 19:09:15 +0000

    add simple layout for blog posts

---
+++ src/_firn/layouts/update.clj
@@ -0,0 +1,21 @@
+(defn update
+  [{:keys [render partials title build-url]}]
+  (let [{:keys [head]} partials]
+    [:html
+     (head build-url)
+     [:body
+      [:main
+       [:article.content
+        ;; [:div (render :toc)] ;; Optional; add a table of contents
+        [:h1 title] 
+        [:div (render :breadcrumbs)]
+        [:div (render :file)]
+        ;; if backlinks exist, store them in a let bindings.
+        (when-let [backlinks (render :backlinks)] 
+          [:div
+           [:hr]
+           [:div.backlinks
+            [:h4 "Backlinks to this document:"]
+            backlinks]])
+        [:hr]
+        [:div.back [:p "Back to blog: " [:a {:href "https://exp2exp.github.io/updates.html"} "Exp2Exp: Updates"]]]]]]]))
