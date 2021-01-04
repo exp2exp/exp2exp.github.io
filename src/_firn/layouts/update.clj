e29e1d8d7f35a6a9b99d2d18a8cfe2a802eb6b28
holtzermann17 <joseph.corneli@hyperreal.enterprises>
2021-01-04 19:19:03 +0000

    bring in date

--- src/_firn/layouts/update.clj
+++ src/_firn/layouts/update.clj
@@ -1,5 +1,5 @@
 (defn update
-  [{:keys [render partials title build-url]}]
+  [{:keys [render partials title date-created build-url]}]
   (let [{:keys [head]} partials]
     [:html
      (head build-url)
@@ -8,6 +8,7 @@
        [:article.content
         ;; [:div (render :toc)] ;; Optional; add a table of contents
         [:h1 title]
+        [:h2 date-created]
         [:div (render :file)]
         ;; if backlinks exist, store them in a let bindings.
         (when-let [backlinks (render :backlinks)] 
