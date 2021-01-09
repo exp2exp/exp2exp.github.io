8c6c7b2eb11d8666d470c51a7c54778c0c6e52d5
holtzermann17 <joseph.corneli@hyperreal.enterprises>
2021-01-09 20:47:08 +0000

    update .dir-locals.el with instructions on setting location

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
-((nil . ((org-roam-directory . (getenv "PATH-TO-EXP2EXP"))
-         (org-roam-db-location . (concat (getenv "PATH-TO-EXP2EXP") "/org-roam.db")))))
+;; Please set the path accordingly, and don’t check in your changes
+;; or they will overwrite the default for everyone else.
+((nil . ((org-roam-directory . "~/exp2exp.github.io/src/")
+         (org-roam-db-location . "~/exp2exp.github.io/src/org-roam.db"))))
