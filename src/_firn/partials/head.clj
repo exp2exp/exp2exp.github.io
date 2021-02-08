89e3400cc6a34f805b4728d5aa05dc86859070e5
holtzermann17 <joseph.corneli@hyperreal.enterprises>
2021-02-08 13:23:28 +0000

    include MathJaX

--- src/_firn/partials/head.clj
+++ src/_firn/partials/head.clj
@@ -1,7 +1,17 @@
 (defn head
   [build-url]
-  [:html
+  [:doctype :html5]
    [:head
     [:meta {:charset "UTF-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-    [:link {:rel "stylesheet" :href (build-url "/static/css/firn_base.css")}]]])
+    [:script {:type "text/x-mathjax-config" (str "<script type=\"text/x-mathjax-config\">
+  MathJax.Hub.Config({
+    tex2jax: {
+      inlineMath: [ ['$','$'], [\"\\(\",\"\\)\"] ],
+      processEscapes: true
+    }
+  });
+</script>")}]
+    [:script {:src "https://polyfill.io/v3/polyfill.min.js?features=es6"}]
+    [:script {:type "text/javascript" :async "async" :src "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"}]
+    [:link {:rel "stylesheet" :href (build-url "/static/css/firn_base.css")}]])
