c93078e078bc7155f8f0934422171b6dd94ce700
Leo Vivier <zaeph@zaeph.net>
2021-03-04 15:05:32 +0100

    Do not force .dir-locals.el
    * .dir-locals.default.el: Create no-op file with instructions
    * .gitignore: Ignore .dir-locals.el

---
+++ .dir-locals.default.el
@@ -0,0 +1,5 @@
+;; 1. Modify this file with the path to your local repo.
+;; 2. Save it as ".dir-locals.el".
+
+((nil . ((org-roam-directory . "~/exp2exp.github.io/src/")
+         (org-roam-db-location . "~/exp2exp.github.io/src/org-roam.db"))))
