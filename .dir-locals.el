f4d5d4257aca105b2e1ca97cf11a047c78b511aa
Leo Vivier <zaeph@zaeph.net>
2021-03-04 15:15:25 +0100

    Delete remaining .dir-locals.el

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-;; Please set the path accordingly, and don’t check in your changes
-;; or they will overwrite the default for everyone else.
-((nil . ((org-roam-directory . "~/exp2exp.github.io/src/")
-         (org-roam-db-location . "~/exp2exp.github.io/src/org-roam.db"))))
