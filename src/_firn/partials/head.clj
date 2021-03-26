6076ed00cbe4e61bb163b6043fed1e71447922b1
holtzermann17 <joseph.corneli@hyperreal.enterprises>
2021-03-26 00:25:50 +0000

    update CSS to use Ubuntu fonts

--- src/_firn/partials/head.clj
+++ src/_firn/partials/head.clj
@@ -14,4 +14,8 @@
     [:script {:src "https://polyfill.io/v3/polyfill.min.js?features=es6"}]
     [:script {:src "https://hypothes.is/embed.js" :async "async"}]
     [:script {:type "text/javascript" :async "async" :src "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"}]
-    [:link {:rel "stylesheet" :href (build-url "/static/css/firn_base.css")}]])
+    [:link {:rel "stylesheet" :href (build-url "/static/css/firn_base.css")}]
+    [:link {:rel "preconnect" :href "https://fonts.gstatic.com"}]
+    [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&family=Ubuntu+Mono&family=Ubuntu:wght@300;400&display=swap"}]
+])
+
