36898a1b6bd0dc661ab1c6a24e3a8d0ed1057730
holtzermann17 <joseph.corneli@hyperreal.enterprises>
2021-04-20 23:04:03 +0100

    adjust the typescript Up direction link

--- src/_firn/layouts/typescript.clj
+++ src/_firn/layouts/typescript.clj
@@ -22,4 +22,4 @@
             [:h4 "Backlinks to this document:"]
             backlinks]])
         [:hr]
-        [:div.back [:p "Back to blog: " [:a {:href "https://exp2exp.github.io/updates.html"} "Exp2Exp: Updates"]]]]]]]))
+        [:div.back [:p "Back to blog: " [:a {:href "https://exp2exp.github.io/Writing"} "Exp2Exp: Writing"]]]]]]]))
